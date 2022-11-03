f37ecf78757ba46f0a8b6b8cb89a8a0d62a5f88b
Jure <jure.mrzek15@gmail.com>
2022-11-03 17:13:21 +0100

    Importing .json files
    You can now import .json file to the program and it will automatically render that object.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ import {shaders} from './shaders.js'
 const mat4 = glMatrix.mat4;
 
 class Game extends Application {
-    start(){
-        //Create any variables
-        const gl = this.gl;
+    async start(){
+        //Create global variables
         this.player = new Player();
-        const player = this.player; //so we don't have to call this every time
         this.cameraEye = [1,1,1]
         this.time = performance.now();
         this.startTime = this.time;
-        
+
+        //Local variables so we don't have to call this every time
+        const gl = this.gl;
+        const player = this.player;
+
+        //---------WebGL initialization-----------------------------------------------------------------------------------------
         //Create vertex shader
         const vertexShader = gl.createShader(gl.VERTEX_SHADER);
         gl.shaderSource(vertexShader, shaders.shader1.vertex); //set string source from shaders.js
@@ -39,27 +42,20 @@ class Game extends Application {
             throw new Error('Cannot link program\nInfo log:\n' + log);
         }
 
-        //Start program
+        //Start the program and enable depth test
         gl.useProgram(program);
-
         gl.enable(gl.DEPTH_TEST);
         gl.enable(gl.CULL_FACE);
         gl.depthFunc(gl.LESS);
- 
-        //3 BUFFERJI - Vertices, indices (triangle index) and colors
-        const vertices = new Float32Array([ 
-            //xyzw position      //rgb color
-            -1, -1, -1,     0, 0, 0,
-            -1, -1,  1,     0, 0, 1,
-            -1,  1, -1,     0, 1, 0,
-            -1,  1,  1,     0, 1, 1,
-             1, -1, -1,     1, 0, 0,
-             1, -1,  1,     1, 0, 1,
-             1,  1, -1,     1, 1, 0,
-             1,  1,  1,     1, 1, 1,
-        ]);
-        //this.vao = gl.createVertexArray();
-        //gl.bindVertexArray(this.vao);
+        //-------------------------------------------------------------------------------------------------------------------------------------
+        
+        //Load json file
+        const response = await fetch('./cow.json');
+        const h = await response.json();
+        const json = [JSON.stringify(h)]
+        const pyramidObject = JSON.parse(json);
+
+        const vertices = new Float32Array(pyramidObject.meshes[0].vertices);
 
         const verticesBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer);
@@ -69,79 +65,73 @@ class Game extends Application {
         gl.enableVertexAttribArray(aPositionLoc);
         gl.vertexAttribPointer(
             aPositionLoc, //attribute location
-            3, //število komponent
-            gl.FLOAT, //tip komponent
-            false, //normalize
-            6*4, //stride - velikost posameznega bloka podatkov v arrayu - kjer se podatki začnejo ponovno pošiljati za drug objekt.
-            0, //offset - offset v bloku. Na prvem atributu je 0, potem pa treba incrementat, drugače bi overwritali data.
+            3,            //število komponent
+            gl.FLOAT,     //tip komponent
+            false,        //normalize
+            3*4,          //stride - velikost posameznega bloka podatkov v arrayu - kjer se podatki začnejo ponovno pošiljati za drug objekt.
+            0,            //offset - odmik v bloku. Na prvem atributu je 0, potem pa treba incrementat, drugače bi overwritali data.
         );
 
-        const aColorLoc = gl.getAttribLocation(program, 'aColor');
-        gl.enableVertexAttribArray(aColorLoc);
-        gl.vertexAttribPointer(aColorLoc, 3, gl.FLOAT, false, 6*4, 3*4);
-        
-        const indices = new Uint16Array([
-            0, 1, 2,    2, 1, 3,
-            4, 0, 6,    6, 0, 2,
-            5, 4, 7,    7, 4, 6,
-            1, 5, 3,    3, 5, 7,
-            6, 2, 7,    7, 2, 3,
-            1, 0, 5,    5, 0, 4,
-        ]);
-        
+        //const aColorLoc = gl.getAttribLocation(program, 'aColor');
+        //gl.enableVertexAttribArray(aColorLoc);
+        //gl.vertexAttribPointer(aColorLoc, 3, gl.FLOAT, false, 6*4, 3*4);
+        const indices = new Uint16Array(pyramidObject.meshes[0].faces.flat(1));
+        this.elementCount = indices.length;
         this.indicesBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indicesBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
         //CAMERA-----------------------------------------------------------------------------------------------------------------------------
         this.modelMatrix = mat4.create();
-        this.cameraMatrix = mat4.create();
+        this.viewMatrix = mat4.create();
         this.projectionMatrix = mat4.create();
         mat4.scale(this.modelMatrix, this.modelMatrix, [.5, .5, .5]);
-        //Matrika, [x, y, z] lokacija kamere, [x, y, z] kam gleda kamera, up <- vse relative na svet!!
-        mat4.lookAt(this.cameraMatrix, [-1, 1, 0], this.player.pos, [0, 1, 0]);
+        //Matri, [x, y, z] lokacija kamere, [x, y, z] kam gleda kamera, up <- vse absolutne koordinate!
+        mat4.lookAt(
+            this.viewMatrix, //Matrika kamera
+            [-1, 1, 0],        //Lokacija kamere
+            player.pos,   //V katero točko naj gleda kamera
+            [0, 1, 0]          //Katera smer je gor
+        );
         mat4.perspective(this.projectionMatrix, Math.PI/1.5, gl.canvas.width/gl.canvas.height, 0.0001, 20);
 
         //Event listeners
         window.addEventListener('keydown', (event) => {
             if(event.key == "w" || event.key == "W")
-                this.player.forward = true;
+                player.forward = true;
             if(event.key == "s" || event.key == "A")
-                this.player.backwards = true;
+                player.backwards = true;
             if(event.key == "a" || event.key == "S")
-                this.player.left = true;
+                player.left = true;
             if(event.key == "d" || event.key == "D")
-                this.player.right = true;
+                player.right = true;
             if(event.key == "ArrowLeft")
-                this.player.rotateLeft = true;
+                player.rotateLeft = true;
             if(event.key == "ArrowRight")
-                this.player.rotateRight = true;
+                player.rotateRight = true;
         });
         window.addEventListener('keyup', (event) => {
             if(event.key == "w" || event.key == "W")
-                this.player.forward = false;
+                player.forward = false;
             if(event.key == "s" || event.key == "A")
-                this.player.backwards = false;
+                player.backwards = false;
             if(event.key == "a" || event.key == "S")
-                this.player.left = false;
+                player.left = false;
             if(event.key == "d" || event.key == "D")
-                this.player.right = false;
+                player.right = false;
             if(event.key == "ArrowLeft")
-                this.player.rotateLeft = false;
+                player.rotateLeft = false;
             if(event.key == "ArrowRight")
-                this.player.rotateRight = false;
+                player.rotateRight = false;
         });
 
-        //Matrike pošljemo v shader kot uniforme
-        this.uModelMatrixLoc = gl.getUniformLocation(program, 'uModelMatrix');
-        this.uCameraMatrixLoc = gl.getUniformLocation(program, 'uCameraMatrix');
-        this.uProjectionMatrixLoc = gl.getUniformLocation(program, 'uProjectionMatrix');
-        gl.uniformMatrix4fv(this.uModelMatrixLoc, false, this.modelMatrix);
-        gl.uniformMatrix4fv(this.uCameraMatrixLoc, false, this.cameraMatrix);
-        gl.uniformMatrix4fv(this.uProjectionMatrixLoc, false, this.projectionMatrix);
+        //Izračunamo MVP matriko in pošljemo v vertex shader
+        this.uMvpMatrixLoc = gl.getUniformLocation(program, 'uMvpMatrix');
+        this.mvpMatrix = getMvpMatrix(this.modelMatrix, this.viewMatrix, this.projectionMatrix);
+        gl.uniformMatrix4fv(this.uMvpMatrixLoc, false, this.mvpMatrix);
 
         //Draw triangles - each point in bufferData is a vertex.
-        gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, this.elementCount, gl.UNSIGNED_SHORT, 0);
     }
     update(){
         //get dt so that the game is equally fast regardless of device performance
@@ -153,38 +143,39 @@ class Game extends Application {
         const player = this.player;
         player.direction = [0,0,0];
         //PLAYER MOVEMENT
-        if(this.player.forward){
+        if(player.forward){
             player.direction[0] += player.speed*dt;
         }
-        if(this.player.backwards){
+        if(player.backwards){
             player.direction[0] -= player.speed*dt;
         }
-        if(this.player.left){
+        if(player.left){
             player.direction[2] -= player.speed*dt;
         }
-        if(this.player.right){
+        if(player.right){
             player.direction[2] += player.speed*dt;
         }
-        if(this.player.rotateLeft){
+        if(player.rotateLeft){
             mat4.rotateY(this.modelMatrix, this.modelMatrix, player.rotateSpeed*dt);
         }
-        if(this.player.rotateRight){
+        if(player.rotateRight){
             mat4.rotateY(this.modelMatrix, this.modelMatrix, -player.rotateSpeed*dt);
         }
         mat4.translate(this.modelMatrix, this.modelMatrix, player.direction);
+        mat4.getTranslation(player.pos, this.modelMatrix);
 
-        mat4.getTranslation(this.player.pos, this.modelMatrix);
-        gl.uniformMatrix4fv(this.uModelMatrixLoc, false, this.modelMatrix)
-        //mat4.lookAt(this.cameraMatrix, this.cameraEye, player.pos, [0, 1, 0]);
-        //gl.uniformMatrix4fv(this.uCameraMatrixLoc, false, this.cameraMatrix);
-        gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+        //send mvp matrix to vertex shader
+        this.mvpMatrix = getMvpMatrix(this.modelMatrix, this.viewMatrix, this.projectionMatrix);
+        gl.uniformMatrix4fv(this.uMvpMatrixLoc, false, this.mvpMatrix);
+
+        //mat4.lookAt(this.viewMatrix, this.cameraEye, player.pos, [0, 1, 0]);
+        //gl.uniformMatrix4fv(this.uviewMatrixLoc, false, this.viewMatrix);
     }
     render(){
         const gl = this.gl;
-        gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, this.elementCount, gl.UNSIGNED_SHORT, 0);
     }
 }
-
   
 // resize the canvas to fill browser window dynamically
 function resizeCanvas() {
@@ -206,4 +197,10 @@ function toRadians(degrees){
 }
 function toDegrees(radians){
     return radians * (180/Math.PI);
+}
+function getMvpMatrix(modelMatrix, viewMatrix, projectionMatrix){
+    let mvpMatrix = mat4.create();
+    mat4.multiply(mvpMatrix, projectionMatrix, viewMatrix);
+    mat4.multiply(mvpMatrix, mvpMatrix, modelMatrix);
+    return mvpMatrix;
 }
--- js/shaders.js
+++ js/shaders.js
@@ -1,27 +1,23 @@
 const vertex = `#version 300 es
-    uniform mat4 uModelMatrix;
-    uniform mat4 uCameraMatrix;
-    uniform mat4 uProjectionMatrix;
+    uniform mat4 uMvpMatrix;
 
     in vec4 aPosition;
-    in vec4 aColor;
+    //in vec4 aColor;
 
-    out vec4 vColor;
+    //out vec4 vColor;
     
     void main(){
-        vColor = aColor;
-        gl_Position = uProjectionMatrix*uCameraMatrix*uModelMatrix*aPosition;
+        gl_Position = uMvpMatrix*aPosition;
     }
 `;
 const fragment = `#version 300 es
     precision mediump float;
-    in vec4 vColor;
+    //in vec4 vColor;
 
     out vec4 oColor;
 
     void main(){
-        //oColor = vec4(0,1,0,1);
-        oColor = vColor;
+        oColor = vec4(.5,.5,.5,1);
     }
 `;
 export const shaders = {
